e359ed805e36ee15d60d2ddff05db60b6c58468e
Tomas Godoybaca <tommygodoybaca@outlook.com>
2025-12-09 15:41:02 -0600

    added way to display the array for the card groups and subjects - no interactions yet - no css yet

--- flash_cards/src/App.tsx
+++ flash_cards/src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 //flashcards
 //create / delete / edit cards
@@ -27,14 +27,44 @@ interface CardNextButtonProps {
   cardArray: object[];
 }
 
-function CardNextButton({ cardIndex, setCardIndex, cardArray }: CardNextButtonProps) {
+interface Subject {
+  title: string;
+  cardGroups: CardSet[];
+}
+
+interface CardSet {
+  title: string;
+  dateCreated: number;
+  dateEdited: number;
+  cards: Card[];
+}
+
+interface Card {
+  question: string;
+  correctAnswer: string;
+  incorrectAnswers: string[];
+}
+
+function CardNextButton({
+  cardIndex,
+  setCardIndex,
+  cardArray,
+}: CardNextButtonProps) {
   //if the user is at the last card
   //TODO: add a different state when the user hasn't put in an answer vs when they have. ie. "submit" or "skip"
-  if (cardIndex == (cardArray.length - 1)) {
+  if (cardIndex == cardArray.length - 1) {
     //onClick run function that needs to run whrn the user if finished with a "book"
-    return <button>Finish</button>
+    return <button>Finish</button>;
   } else {
-    return <button onClick={() => { setCardIndex(cardIndex + 1) }}>Submit</button>
+    return (
+      <button
+        onClick={() => {
+          setCardIndex(cardIndex + 1);
+        }}
+      >
+        Submit
+      </button>
+    );
   }
 }
 
@@ -43,60 +73,132 @@ function CardNextButton({ cardIndex, setCardIndex, cardArray }: CardNextButtonPr
 function handleSubmit(correctAnswer: string, givenAnswer: string) {
   correctAnswer = correctAnswer.toLowerCase();
   givenAnswer = givenAnswer.toLowerCase();
-  return (correctAnswer === givenAnswer);
+  return correctAnswer === givenAnswer;
 }
-
 
 //how the overall object will be laid out
 //might change if I decide to add the settings to the object aswell
 
-let fillerData = [
+let fillerData: Subject[] = [
   {
-    "subject": "science",
-    "cardGroups": [{
-      "title": "test 1",
-      "cards": [
-        {
-          "question": "What colour is the sky?",
-          "correct": "blue",
-          "incorrect": ["red", "green", "yellow"]
-        },
-        {
-          "question": "What colour is the grass?",
-          "correct": "green",
-          "incorrect": ["red", "blue", "yellow"]
-        },
-      ]
-    }
-
-    ]
-  }
-]
+    title: "science",
+    cardGroups: [
+      {
+        title: "test 1",
+        dateCreated: 1,
+        dateEdited: 1,
+        cards: [
+          {
+            question: "What colour is the sky?",
+            correctAnswer: "blue",
+            incorrectAnswers: ["red", "green", "yellow"],
+          },
+          {
+            question: "What colour is the grass?",
+            correctAnswer: "green",
+            incorrectAnswers: ["red", "blue", "yellow"],
+          },
+        ],
+      },
+      {
+        title: "test 2",
+        dateCreated: 1,
+        dateEdited: 1,
+        cards: [
+          {
+            question: "What colour is the sky?",
+            correctAnswer: "blue",
+            incorrectAnswers: ["red", "green", "yellow"],
+          },
+          {
+            question: "What colour is the grass?",
+            correctAnswer: "green",
+            incorrectAnswers: ["red", "blue", "yellow"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "math",
+    cardGroups: [
+      {
+        title: "test 1",
+        dateCreated: 1,
+        dateEdited: 1,
+        cards: [
+          {
+            question: "What colour is the sky?",
+            correctAnswer: "blue",
+            incorrectAnswers: ["red", "green", "yellow"],
+          },
+          {
+            question: "What colour is the grass?",
+            correctAnswer: "green",
+            incorrectAnswers: ["red", "blue", "yellow"],
+          },
+        ],
+      },
+    ],
+  },
+];
 
 function FlashCard() {
   const [cardIndex, setCardIndex] = useState(0);
   const [cardAnswer, setCardAnswer] = useState("");
 
-  const correctAnswer = fillerData[0].cardGroups[0].cards[cardIndex].correct;
+  const correctAnswer =
+    fillerData[0].cardGroups[0].cards[cardIndex].correctAnswer;
 
   //TODO: change to only shuffle when a new card is chosen.
-  const shuffledAnswers = shuffleArray([fillerData[0].cardGroups[0].cards[cardIndex].correct, ...fillerData[0].cardGroups[0].cards[cardIndex].incorrect])
-  return <>
-    <h3>{fillerData[0].cardGroups[0].cards[cardIndex].question}</h3>
-    {shuffledAnswers.map((answer) => {
-      return <button onClick={() => setCardAnswer(answer)}>{answer}</button>
-    })}
-    <CardNextButton cardIndex={cardIndex} setCardIndex={setCardIndex} cardArray={fillerData[0].cardGroups[0].cards} />
-  </>
+  const shuffledAnswers = shuffleArray([
+    fillerData[0].cardGroups[0].cards[cardIndex].correctAnswer,
+    ...fillerData[0].cardGroups[0].cards[cardIndex].incorrectAnswers,
+  ]);
+  return (
+    <>
+      <h3>{fillerData[0].cardGroups[0].cards[cardIndex].question}</h3>
+      {shuffledAnswers.map((answer) => {
+        return <button onClick={() => setCardAnswer(answer)}>{answer}</button>;
+      })}
+      <CardNextButton
+        cardIndex={cardIndex}
+        setCardIndex={setCardIndex}
+        cardArray={fillerData[0].cardGroups[0].cards}
+      />
+    </>
+  );
+}
+
+function HomePage({ subjectData }: { subjectData: Subject[] }) {
+  let homeSections = subjectData.map((subject, subjectIndex, subjectArray) => (
+    <>
+      <p>{subject.title}</p>
+      {subjectIndex === subjectArray.length - 1 && <p>Last Subject</p>}
+      {subject.cardGroups.map((cardGroup, cardGroupIndex, cardGroupArray) => (
+        <>
+          <p>{cardGroup.title}</p>
+          <p>Created: {cardGroup.dateCreated}</p>
+          <p>Last Edited: {cardGroup.dateEdited}</p>
+          {cardGroupIndex === cardGroupArray.length - 1 && (
+            <p>Last Card Group</p>
+          )}
+        </>
+      ))}
+    </>
+  ));
+  return <>{homeSections}</>;
 }
 
 function App() {
+  const [subjects, updateSubjects] = useState(fillerData);
 
   return (
     <>
-      <FlashCard />
+      {/* <FlashCard /> */}
+      <HomePage subjectData={subjects} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
